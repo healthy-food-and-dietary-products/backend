a860faad3117bec2e9a93506866d3ee3d440643c
Артём Говорливых <govorlivyh@bk.ru>
2023-10-15 23:01:08 +0300

    Создал модель юзера, настроил ему админку.

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,3 +1,35 @@
-# from django.contrib import admin
+from django.contrib import admin
 
-# Register your models here.
+from .models import User
+
+
+class UserAdmin(admin.ModelAdmin):
+    fields = [
+        'username',
+        'is_staff',
+        'is_active',
+        'role',
+        'first_name',
+        'last_name',
+        'email',
+        'date_joined',
+        'last_login',
+        'location',
+        'birth_date',
+    ]
+
+    list_display = [
+        'id',
+        'username',
+        'is_staff',
+        'is_active',
+        'role',
+        'email',
+        'date_joined',
+        'last_login',
+    ]
+    search_fields = ('role',)
+    ordering = ('id',)
+
+
+admin.site.register(User, UserAdmin)
--- backend/users/models.py
+++ backend/users/models.py
@@ -1,3 +1,81 @@
-# from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class User(AbstractUser):
+    """
+    Расширение встроенной модели User."""
+    USER = 'user'
+    MODERATOR = 'moderator'
+    ADMIN = 'admin'
+
+    CHOISES = [
+        (USER, 'Аутентифицированный пользователь'),
+        (MODERATOR, 'Модератор'),
+        (ADMIN, 'Администратор'),
+    ]
+
+    username: str = models.CharField(
+        'Username',
+        unique=True,
+        max_length=150,
+    )
+    email: str = models.EmailField(
+        'E-mail address',
+        unique=True,
+        blank=False,
+        max_length=254,
+    )
+    role: str = models.CharField(
+        max_length=9,
+        choices=CHOISES,
+        default='user'
+    )
+    first_name: str = models.CharField(
+        'first name',
+        max_length=150,
+        blank=True
+    )
+    last_name: str = models.CharField(
+        'last name',
+        max_length=150,
+        blank=True
+    )
+    date_joined = models.DateTimeField(
+        "date joined",
+        default=timezone.now
+    )
+    last_login = models.DateTimeField(
+        'last_login',
+        blank=True,
+        null=True
+    )
+    location: str = models.CharField(
+        'your city',
+        max_length=30,
+        blank=True
+    )
+    birth_date = models.DateField(
+        'birth_date',
+        null=True,
+        blank=True
+    )
+
+    class Meta:
+        ordering = ('id',)
+
+    def __str__(self):
+        return self.username
+
+    @property
+    def is_moderator(self):
+        return self.role == User.MODERATOR
+
+    @property
+    def is_admin(self):
+        return self.role == User.ADMIN
+
+    @property
+    def is_user(self):
+        return self.role == User.USER
