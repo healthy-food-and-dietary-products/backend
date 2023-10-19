46a0ab09f2bfa791dab62b5346efe9bddac438a2
Юлиана Стрельникова <julianka.str@gmail.com>
2023-10-19 09:37:41 +0300

    add shopping cart admin

---
+++ backend/orders/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+
+from orders.models import ShoppingCart, Order
+
+
+@admin.register(ShoppingCart)
+class ShoppingCartAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'product', 'quantity', 'status')
+    list_editable = ('user', 'product', 'quantity')
+    search_fields = ('user',)
+
+
