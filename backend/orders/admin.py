a5e439391c3572635bbcaee93af644911b57d2b3
Юлиана Стрельникова <julianka.str@gmail.com>
2023-10-19 18:17:02 +0300

    fix flake8

--- backend/orders/admin.py
+++ backend/orders/admin.py
@@ -8,3 +8,12 @@ class ShoppingCartAdmin(admin.ModelAdmin):
     list_display = ("id", "user", "product", "quantity", "status")
     list_editable = ("user", "product", "quantity")
     search_fields = ("user",)
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'goods',
+                    'date', 'status', 'payment_method', 'is_paid',
+                    'delivery_method', 'comment', 'total_price')
+    list_editable = ('status', 'payment_method',
+                     'is_paid', 'comment', 'delivery_method')
