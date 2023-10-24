0653fa1a6f2a0e77bb5a877657f363c1f3c23428
earlinn <earlinndrow@gmail.com>
2023-10-24 22:16:19 +0600

    Move Djoser urls to the api folder

--- backend/good_food/urls.py
+++ backend/good_food/urls.py
@@ -5,8 +5,7 @@ from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("auth/", include("djoser.urls")),
-    path("auth/", include("djoser.urls.authtoken")),
+    path("api/", include("api.users_urls")),
 ]
 
 
