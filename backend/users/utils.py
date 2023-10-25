6479dbd82b14d2e19f3c4bec53c052866e6b7750
Артём Говорливых <govorlivyh@bk.ru>
2023-10-25 23:17:00 +0300

    Technical commit, need for pull develop to me

---
+++ backend/users/utils.py
@@ -0,0 +1,12 @@
+city_choices = [
+    ('St. Petersburg', "Санкт-Петербург"),
+    ('Leningrad region', 'Ленинградская область'),
+    ('Moscow', 'Москва'),
+    ('Moscow region', 'Московская область'),
+]
+
+city_type_choices = [
+    ('City', "Город"),
+    ('Settlement', 'Поселок'),
+    ('Village', 'Деревня'),
+]
