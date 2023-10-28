b056763a7da2b911dc5efca22c6fee4a6fde2c21
Юлиана Стрельникова <julianka.str@gmail.com>
2023-10-28 12:34:13 +0300

    fix urls

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
+from .orders_views import OrderViewSet, ShoppingCartViewSet
 from .products_views import (
     CategoryViewSet,
     ComponentViewSet,
@@ -13,9 +14,13 @@ from .products_views import (
 )
 from .users_views import AddressViewSet, UserViewSet
 
+app_name = "api"
+
 router = DefaultRouter()
 router.register("addresses", AddressViewSet)
 router.register("users", UserViewSet)
+router.register("shopping_cart", ShoppingCartViewSet),
+router.register("order", OrderViewSet)
 router.register("categories", CategoryViewSet)
 router.register("subcategories", SubcategoryViewSet)
 router.register("components", ComponentViewSet)
@@ -25,8 +30,9 @@ router.register("promotions", PromotionViewSet)
 router.register("products", ProductViewSet)
 router.register("favorites", FavoriteProductViewSet)
 
+
 urlpatterns = [
+    path("", include(router.urls)),
     path("auth/", include("djoser.urls")),
     path("auth/", include("djoser.urls.authtoken")),
-    path("", include(router.urls)),
 ]
