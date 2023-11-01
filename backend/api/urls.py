b46923caaa6412e618ba2ff92a03732b892e3ad8
earlinn <earlinndrow@gmail.com>
2023-11-01 22:01:33 +0600

    Revert disabled urls

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,7 +1,7 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-# from .orders_views import ShoppingCartViewSet
+from .orders_views import ShoppingCartViewSet
 from .products_views import (
     CategoryViewSet,
     ComponentViewSet,
@@ -12,14 +12,13 @@ from .products_views import (
     SubcategoryViewSet,
     TagViewSet,
 )
-
-# from .users_views import AddressViewSet
+from .users_views import AddressViewSet
 
 app_name = "api"
 
 router = DefaultRouter()
 router.register("categories", CategoryViewSet)
-# router.register("shopping_cart", ShoppingCartViewSet),
+router.register("shopping_cart", ShoppingCartViewSet),
 router.register("subcategories", SubcategoryViewSet)
 router.register("components", ComponentViewSet)
 router.register("tags", TagViewSet)
@@ -27,9 +26,9 @@ router.register("producers", ProducerViewSet)
 router.register("promotions", PromotionViewSet)
 router.register("products", ProductViewSet)
 router.register("favorite-products", FavoriteProductViewSet)
-# router.register(
-#     r"users/(?P<user_id>\d+)/addresses", AddressViewSet, basename="addresses"
-# )
+router.register(
+    r"users/(?P<user_id>\d+)/addresses", AddressViewSet, basename="addresses"
+)
 
 urlpatterns = [
     path("", include(router.urls)),
