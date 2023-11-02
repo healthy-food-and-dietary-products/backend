cae643c8dbbb0e4b1f18dfa358dc76bc6853bc30
earlinn <earlinndrow@gmail.com>
2023-11-02 12:32:50 +0600

    Move Swagger urls to api level

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,4 +1,7 @@
-from django.urls import include, path
+from django.urls import include, path, re_path
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
 from rest_framework.routers import DefaultRouter
 
 # from .orders_views import ShoppingCartViewSet
@@ -35,4 +38,33 @@ urlpatterns = [
     path("", include(router.urls)),
     path("", include("djoser.urls")),
     path("", include("djoser.urls.authtoken")),
+]
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Good Food API",
+        default_version="v1",
+        description="API documentation for the GoodFood project",
+        # terms_of_service="URL страницы с пользовательским соглашением",
+        contact=openapi.Contact(email="healthyfoodapi@yandex.ru"),
+        license=openapi.License(name="MIT License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
+
+urlpatterns += [
+    re_path(
+        r"^swagger(?P<format>\.json|\.yaml)$",
+        schema_view.without_ui(cache_timeout=0),
+        name="schema-json",
+    ),
+    re_path(
+        r"^swagger/$",
+        schema_view.with_ui("swagger", cache_timeout=0),
+        name="schema-swagger-ui",
+    ),
+    re_path(
+        r"^redoc/$", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"
+    ),
 ]
