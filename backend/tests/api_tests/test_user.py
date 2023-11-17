57a27787ce010545420e81bd4baa49ebca64437f
earlinn <earlinndrow@gmail.com>
2023-11-17 16:29:34 +0600

    Test register user fail

--- backend/tests/api_tests/test_user.py
+++ backend/tests/api_tests/test_user.py
@@ -7,10 +7,26 @@ def test_register_user(client):
     payload = {"username": USERNAME, "email": EMAIL, "password": PASSWORD}
     response = client.post("/api/users/", payload)
 
+    assert response.status_code == 201
     assert response.data["username"] == payload["username"]
     assert response.data["email"] == payload["email"]
     assert response.data["city"] == CITY
     assert "password" not in response.data
+
+
+@pytest.mark.django_db
+def test_register_user_validation_fail(client):
+    payload = [
+        {"username": USERNAME, "email": EMAIL},
+        {"username": USERNAME, "password": PASSWORD},
+        {"email": EMAIL, "password": PASSWORD},
+    ]
+    for field in payload:
+        response = client.post("/api/users/", field)
+
+        assert response.status_code == 200
+        assert response.data["type"] == "validation_error"
+        assert response.data["errors"][0]["code"] == "required"
 
 
 @pytest.mark.django_db
@@ -29,6 +45,13 @@ def test_login_user_fail(client):
     assert response.status_code == 400
     assert response.data["type"] == "validation_error"
     assert response.data["errors"][0]["code"] == "invalid_credentials"
+
+
+@pytest.mark.django_db
+def test_logout_user(auth_client):
+    response = auth_client.post("/api/token/logout/")
+
+    assert response.status_code == 204
 
 
 @pytest.mark.django_db
@@ -82,10 +105,3 @@ def test_delete_me_anonymous_fail(client):
     assert response.status_code == 401
     assert response.data["type"] == "client_error"
     assert response.data["errors"][0]["code"] == "not_authenticated"
-
-
-@pytest.mark.django_db
-def test_logout_user(auth_client):
-    response = auth_client.post("/api/token/logout/")
-
-    assert response.status_code == 204
