b59d5c8eb8206bf9457384b334d3a8f88b2718c7
Артём Говорливых <govorlivyh@bk.ru>
2023-11-20 22:49:47 +0300

    fix pnone number validation

--- backend/users/models.py
+++ backend/users/models.py
@@ -3,8 +3,7 @@ from django.core.exceptions import ValidationError
 from django.db import models
 from django.utils import timezone
 from django_cleanup import cleanup
-from phonenumber_field.modelfields import PhoneNumberField
-
+from django.core.validators import RegexValidator
 from users import utils
 
 
@@ -22,7 +21,8 @@ class User(AbstractUser):
         "City", choices=utils.city_choices, max_length=50, default="Moscow"
     )
     birth_date = models.DateField("Birth_date", blank=True, null=True)
-    phone_number = PhoneNumberField("Phone_number", blank=True)
+    phone_regex = RegexValidator(regex=r'^\+?1?\d{11}$', message="Введен некорректный номер телефона. Введите номер телефона в формате '+79999999999'.")
+    phone_number = models.CharField(validators=[phone_regex], max_length=17, blank=True)
     photo = models.ImageField(
         "Photo",
         upload_to=user_directory_path,
