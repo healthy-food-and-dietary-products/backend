f17c5abdcc44003a3ff086cfc40596a12b98d4f8
Юлиана Стрельникова <julianka.str@gmail.com>
2023-11-30 16:40:22 +0300

    fix order tests

--- backend/tests/api_tests/test_order.py
+++ backend/tests/api_tests/test_order.py
@@ -6,7 +6,6 @@ from orders.models import Order
 from tests.fixtures import ADDRESS1, FIRST_NAME, LAST_NAME, PHONE_NUMBER, USER_EMAIL
 
 
-@pytest.mark.skip(reason="Not passing now, need to fix")
 @pytest.mark.django_db(transaction=True)
 class TestOrder:
 
@@ -34,35 +33,13 @@ class TestOrder:
         )
         return response.json()
 
-    def test_create_order_auth_client(
-        self, auth_client, user, products, delivery_points
-    ):
-        order_data = {
-            "payment_method": "Payment at the point of delivery",
-            "delivery_method": "Point of delivery",
-            "package": 0,
-            "comment": "",
-            "delivery_point": delivery_points.id,
-        }
-        self.create_shopping_cart_authorized(auth_client, products)
-        response = auth_client.post(
-            "/api/order/", order_data, format="json"
-        )
-        assert response.status_code == status.HTTP_201_CREATED
-        order = Order.objects.get()
-        assert user.id == order.user.id
-        assert order.status == "Ordered"
-        assert order.delivery_method == "Point of delivery"
-        assert order.payment_method == "Payment at the point of delivery"
-
-    def test_create_order_another_auth_client(
-        self, auth_client, products, delivery_points
-    ):
+    def test_create_order_auth_client(self, auth_client, products):
         order_data = {
             "payment_method": "In getting by cash",
             "delivery_method": "By courier",
             "package": 200,
-            "comment": "After 14:00"
+            "comment": "After 14:00",
+            "add_address": "Saint-Peterburg"
         }
         self.create_shopping_cart_authorized(auth_client, products)
         response = auth_client.post(
@@ -75,6 +52,7 @@ class TestOrder:
         assert order.payment_method == "In getting by cash"
         assert order.package == 200
         assert order.comment == "After 14:00"
+        assert order.add_address == "Saint-Peterburg"
 
     def test_create_order_anonimus_client(
         self, client, products, delivery_points
@@ -104,14 +82,41 @@ class TestOrder:
         assert order.payment_method == "In getting by cash"
         assert order.user_data == user_data
 
+    def test_create_order_another_anonimus_client(
+        self, client, products, delivery_points
+    ):
+        user_data = (
+            f"first_name: {FIRST_NAME}, "
+            f"last_name: {LAST_NAME},"
+            f" phone_number: {PHONE_NUMBER},"
+            f" email: {USER_EMAIL}"
+        )
+        order_data = {
+            "user_data": user_data,
+            "payment_method": "Payment at the point of delivery",
+            "delivery_method": "Point of delivery",
+            "package": 200,
+            "delivery_point": delivery_points[0].id
+        }
+        self.create_shopping_cart_anonimous(client, products)
+        response = client.post(
+            "/api/order/", order_data, format="json"
+        )
+        assert response.status_code == status.HTTP_201_CREATED
+        order = Order.objects.get()
+        assert order.status == "Ordered"
+        assert order.delivery_method == "Point of delivery"
+        assert order.payment_method == "Payment at the point of delivery"
+        assert order.user_data == user_data
+
     def test_delete_order_auth_client(
             self, auth_client, products, delivery_points):
         order_data = {
-            "payment_method": "Payment at the point of delivery",
-            "delivery_method": "Point of delivery",
-            "package": 0,
-            "comment": "",
-            "delivery_point": delivery_points.id,
+            "payment_method": "In getting by cash",
+            "delivery_method": "By courier",
+            "package": 200,
+            "comment": "After 14:00",
+            "add_address": "Saint-Peterburg"
         }
         self.create_shopping_cart_authorized(auth_client, products)
         auth_client.post("/api/order/", order_data, format="json")
@@ -125,17 +130,43 @@ class TestOrder:
         self, auth_client, user1, user, delivery_points, products
     ):
         order_data = {
-            "payment_method": "Payment at the point of delivery",
-            "delivery_method": "Point of delivery",
-            "package": 0,
-            "comment": "",
-            "delivery_point": delivery_points.id,
+            "payment_method": "In getting by cash",
+            "delivery_method": "By courier",
+            "package": 200,
+            "comment": "After 14:00",
+            "add_address": "Saint-Peterburg"
         }
         self.create_shopping_cart_authorized(auth_client, products)
         auth_client.post("/api/order/", order_data, format="json")
         order = Order.objects.get()
         client = APIClient()
         client.force_authenticate(user=user1)
+        response = client.delete(
+            f"/api/order/{order.id}/", format="json"
+        )
+        assert response.status_code == status.HTTP_403_FORBIDDEN
+
+    def test_delete_order_anonimus_client(self, client, products, delivery_points):
+        user_data = (
+            f"first_name: {FIRST_NAME}, "
+            f"last_name: {LAST_NAME},"
+            f" phone_number: {PHONE_NUMBER},"
+            f" email: {USER_EMAIL}"
+        )
+        order_data = {
+            "user_data": user_data,
+            "payment_method": "In getting by cash",
+            "delivery_method": "By courier",
+            "package": 0,
+            "comment": "",
+            "add_address": ADDRESS1,
+        }
+        self.create_shopping_cart_anonimous(client, products)
+        response = client.post(
+            "/api/order/", order_data, format="json"
+        )
+        assert response.status_code == status.HTTP_201_CREATED
+        order = Order.objects.get()
         response = client.delete(
             f"/api/order/{order.id}/", format="json"
         )
