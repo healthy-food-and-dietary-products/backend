06c085c6d0a6c6b83fc7bf07b138e801eca040ca
earlinn <earlinndrow@gmail.com>
2023-12-28 14:56:32 +0600

    Remove unnecessary print and file

--- backend/tests/api_tests/test_tags.py
+++ backend/tests/api_tests/test_tags.py
@@ -35,7 +35,6 @@ def test_get_tag_by_id(client, tags):
 def test_create_tag(auth_admin):
     payload = {"name": TEST_NAME}
     response = auth_admin.post(reverse("api:tag-list"), payload)
-    print(response.data)
 
     assert response.status_code == 201
     assert response.data["name"] == TEST_NAME
