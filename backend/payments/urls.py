518707b6e0c92d1167e0694d9554e0127b7ce46b
earlinn <earlinndrow@gmail.com>
2024-01-11 16:13:37 +0600

    Fix payment urls, restore deploy protection

--- backend/payments/urls.py
+++ backend/payments/urls.py
@@ -12,7 +12,7 @@ urlpatterns = [
         views.create_checkout_session,
         name="create-checkout-session",
     ),
-    path("payment-good/", views.SuccessView.as_view()),
-    path("payment-bad/", views.CancelledView.as_view()),
+    path("payment-is-processing/", views.SuccessView.as_view()),
+    path("payment-cancelled/", views.CancelledView.as_view()),
     path("webhooks/stripe/", views.stripe_webhook, name="stripe-webhook"),
 ]
